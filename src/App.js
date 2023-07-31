4a651aaae5d467a74363bc3579d31ff63eda605e
Sandra-Allies <sandra.allies.fr@gmail.com>
2023-07-31 17:56:52 +0200

    background + border body + ribbon name ok

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      {/* FOR RIBBON NAME */}
+        <div class="ribbon"><span>Sandra Alliès</span></div>
+
+
+      {/* <h1><span>Portfolio</span></h1> */}
+    </div>
+  );
+}
+
+export default App;
