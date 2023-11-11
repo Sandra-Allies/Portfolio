b53521e234049872e94ff7d5f6648a078d567031
Sandra-Allies <sandra.allies.fr@gmail.com>
2023-11-11 16:58:32 +0100

    suppression de la video en iframe dans about me

--- src/about/about.js
+++ src/about/about.js
@@ -12,7 +12,6 @@ function About() {
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!
                             Ad dolore dignissimos asperiores dicta facere optio quod commodi nam tempore recusandae. Rerum sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio aliquam!
                         </p>
-                        <iframe src="https://www.youtube.com/embed/jfKfPfyJRdk?si=52wRc1tTWMrEnu9U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                     </td>
                 </tr>
             </table>
