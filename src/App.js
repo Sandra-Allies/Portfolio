3980e7869db9cc8c5bac5638023d96706095ade2
Sandra-Allies <sandra.allies.fr@gmail.com>
2023-11-11 14:20:16 +0100

    design tableau et lorem ajouté, test ajout d'iframe pour les vidéo youtube

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import Header from './header/header';
+import About from './about/about';
 
 function App() {
   return (
     <div className="App">
     <Header />
+    <About />
     </div>
   );
 }
